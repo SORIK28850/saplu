81756eb574ae11032c70ecd854851a753d5aaacb
SORIK28850 <ncreador@gmail.com>
2024-03-20 23:05:58 +0100

    clean_vuetify_use

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,13 +6,6 @@ import App from './components/App.vue';
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
 
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
+import Vuetify from './vuetify';
 
-const vuetify = createVuetify({
-    components,
-    directives
-  });
-  
-createApp(App).use(vuetify).mount('#app');
+createApp(App).use(Vuetify).mount('#app');
