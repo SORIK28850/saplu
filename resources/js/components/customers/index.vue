d81edba45bea7d63d89f1a3d4b86e8d47871e8fa
SORIK28850 <ncreador@gmail.com>
2024-03-25 13:06:21 +0100

    add_table_customers

--- resources/js/components/customers/index.vue
+++ resources/js/components/customers/index.vue
@@ -1,25 +1,60 @@
 <template>
-  <div>
-    Hola
-  </div>
+  <v-data-table-server
+    v-model:items-per-page="itemsPerPage"
+    :headers="headers"
+    :items="serverItems"
+    :items-length="totalItems"
+    :loading="loading"
+    item-value="name"
+    @update:options="loadItems"
+  ></v-data-table-server>
 </template>
 
 <script>
 import axios from 'axios';
 
 export default {
-  name: 'CustomersIndex',
-  mounted() {
-    axios.get('http://127.0.0.1:8000/api/customers')
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+  data: () => ({
+    itemsPerPage: 5,
+    headers: [
+        {
+          title: 'ID',
+          align: 'start',
+          sortable: false,
+          key: 'id',
+        },
+        { title: 'Actions', key: 'actions', align: 'end' },
+        { title: 'Name', key: 'name', align: 'end' },
+        { title: 'Phone', key: 'phone', align: 'end' },
+        { title: 'Address', key: 'address', align: 'end' },
+        { title: 'Care', key: 'care', align: 'end' },
+        { title: 'Schedule', key: 'schedule', align: 'end' },
+        { title: 'Pills', key: 'pills', align: 'end' },
+        { title: 'Diet Type', key: 'dietType', align: 'end' },
+        { title: 'Family Phone', key: 'familyPhone', align: 'end' },
+      ],
+    serverItems: [],
+    loading: true,
+    totalItems: 0,
+    currentPage: 1,
+    totalPages: 0,
+  }),
+  methods: {
+    loadItems ({ page, itemsPerPage, sortBy }) {
+      this.loading = true
+      axios.get(`http://127.0.0.1:8000/api/customers`)
+        .then(response => {
+          this.totalItems = response.data.length;
+          this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
+          this.currentPage = page;
+          this.serverItems = response.data.slice((page - 1) * itemsPerPage, page * itemsPerPage);
+          this.loading = false;
+        })
+        .catch(error => {
+          console.error(error);
+          this.loading = false;
+        });
+    },
+  },
 }
 </script>
-
-<style scoped>
-</style>
---
+++ resources/js/components/customers/modalCard.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+      <v-card
+        v-for="(customer, index) in customers"
+        :key="index"
+        class="mb-4"
+      >
+        <v-card-title>
+          {{ customer.name }}
+        </v-card-title>
+        <v-card-subtitle>
+          ID: {{ customer.id }}
+        </v-card-subtitle>
+        <v-card-text>
+          Phone: {{ customer.phone }} <br>
+          Address: {{ customer.address }} <br>
+          Care: {{ customer.care }} <br>
+          Schedule: {{ customer.schedule }} <br>
+          Pills: {{ customer.pills }} <br>
+          Diet Type: {{ customer.dietType }} <br>
+          Family Phone: {{ customer.familyPhone }} <br>
+          Created At: {{ customer.created_at }} <br>
+          Updated At: {{ customer.updated_at }}
+        </v-card-text>
+      </v-card>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'modalCard',
+    data() {
+      return {
+        customers: [],
+      };
+    },
+    mounted() {
+      axios.get('http://127.0.0.1:8000/api/customers')
+        .then(response => {
+          this.customers = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+  </script>
+  
+  <style scoped>
+  </style>
