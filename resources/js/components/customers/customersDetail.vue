2268e2f7928539758b935bdd452346e4249ce197
SORIK28850 <ncreador@gmail.com>
2024-04-01 18:14:02 +0200

    pfd

--- resources/js/components/customers/customersDetail.vue
+++ resources/js/components/customers/customersDetail.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container class="fill-height" fluid v-if="item">
-      <v-row align="center" justify="center">
+      <v-row align="center" justify="center" id="pdf-content">
         <v-col cols="10">
           <v-card>
             <img
@@ -49,6 +49,8 @@
             </v-card>
 
             <v-card-actions>
+              <v-btn @click="generatePDF" success>Descargar PDF</v-btn>
+
               <v-spacer></v-spacer>
   
               <v-btn
@@ -64,6 +66,8 @@
   
   <script>
 import axios from 'axios';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 export default {
   name: 'customerDetail',
@@ -75,6 +79,26 @@ export default {
   methods: {
     goBack() {
       this.$router.go(-1);
+    },
+    async generatePDF() {
+      this.$nextTick(() => {
+        setTimeout(async () => {
+          const element = document.querySelector('#pdf-content');
+          const imgElement = element.querySelector('.customer-photo');
+          const response = await fetch(imgElement.src);
+          const blob = await response.blob();
+          imgElement.src = URL.createObjectURL(blob);
+          html2canvas(element).then((canvas) => {
+            const imgData = canvas.toDataURL('image/png');
+            const pdf = new jsPDF('p', 'mm', 'a4');
+            const imgProps= pdf.getImageProperties(imgData);
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+            pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+            pdf.save(`${this.item.name}.pdf`);
+          });
+        }, 1000);
+      });
     },
     loadItem() {
       axios.get('https://www.sap-lu.com/api/customers-detail', {
