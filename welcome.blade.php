d314d62e71706a0b18320182ca74e60bae05a75c
SORIK28850 <ncreador@gmail.com>
2024-04-03 14:15:40 +0200

    deployed_ci

---
+++ welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sap-Lu</title>
+	<link rel="stylesheet" href="https://www.sap-lu.com/build/assets/app-Dv_9Ye_M.css">
+</head>
+
+<body>
+    <div id="app">
+    </div>
+	<script src="https://www.sap-lu.com/build/assets/app-Yr1cmxOu.js"></script>
+</body>
+
+</html>
